df3840ba239d81a28d0586174d9a8c008f01df7e
David Song <songdavid98@gmail.com>
2016-03-03 21:08:37 -0500

    more stuff

--- hw04/bounce.js
+++ hw04/bounce.js
@@ -3,38 +3,18 @@ console.log("hello david");
 var startbutton = document.getElementById("start");
 var stopbutton = document.getElementById("stop");
 var clearbutton = document.getElementById("clear");
-
-var CX = 250;
-var CY = 250;
-var circle;
-var radius = 1;
-var growing = 1;
-
-var setup = function setup() {
-    radius = 1;
-    growing = 1;
-    circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    circle.setAttribute("cx", CX);
-    circle.setAttribute("cy", CY);
-    svg.appendChild(circle);
-};
-
 var svg = document.getElementById("svg1");
 
+var img;
+var setup = function setup() {
 
-var drawCircle = function(e) {
-    if (radius >= 250 || radius <= 0)  {
-	growing *= -1;
-    }
-    radius += growing;
-    circle.setAttribute("r", radius);
 };
 
 var interval = 0;
 var start = function start() {
     clear();
     setup();
-    interval = setInterval(drawCircle, 16);
+    interval = setInterval(bounce, 16);
 };
 var stop = function stop() {
     clearInterval(interval);
