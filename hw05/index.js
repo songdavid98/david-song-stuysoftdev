03cae28382c3f8e304b0b0014edf20488f66c868
David Song <songdavid98@gmail.com>
2016-03-19 11:00:27 -0400

    more updates

--- hw05/index.js
+++ hw05/index.js
@@ -1,11 +1,11 @@
 //JAVASCRIPT
 
-var info = [1, 2, 3];
+var info = [[1, "iowa"], [2, "new hampshire"], [3, "south carolina"]];
 
 d3.select(".chart")
   .selectAll("div")
     .data(info)
     .enter().append("div")
     .style("background-color", "pink")
-    .style("width", function(d) { return d * 10 + "px"; })
-    .text(function(d) { return d; });
+    .style("width", function(d) { return d[0] * 10 + "px"; })
+    .text(function(d) { return d[1] + ": " + d[0]; });
