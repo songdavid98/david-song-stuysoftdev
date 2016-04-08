538b4521efe9c4cf38d4005852bdb9d6c2cd196f
David Song <songdavid98@gmail.com>
2016-04-08 13:16:54 -0400

    finished hw09

---
+++ hw09/hw09.py
@@ -0,0 +1,38 @@
+#hw09
+
+import time
+
+def wrapper( f ):
+   
+      
+   def inner( *arg ):
+      stime = time.time()
+      z = f( *arg )
+      etime = time.time()
+      print f.func_name + " time: " + str(etime - stime)
+      return z
+   
+   
+   return inner
+
+@wrapper
+def foo(*args):
+   i = 1
+   lols = "lols"
+   nana = "nana"
+   for a in args:
+      print a
+      print "argument " + str(i) + ": "
+      
+      lols *= i
+      print lols
+      print "trol" + lols
+      i += 1
+      
+      nana *= i
+      print nana
+      print ""
+   print "batman"
+   print ""
+
+foo( -2, 3, 'hello' )
